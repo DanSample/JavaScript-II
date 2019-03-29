b477224f0faf97609df625af3a766a8b60883bf2
Dan Sample <danssample@gmail.com>
2019-03-29 15:41:32 -0700

    callbacks.js challenges

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -26,8 +26,11 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
+  return cb(arr.length);
   // getLength passes the length of the array into the callback.
 }
+
+console.log(getLength(items));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
